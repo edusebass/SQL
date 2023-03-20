56e69722b73a9c2a531f7cef9d5064a172262e0f
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 16:00:58 -0500

    Update and rename 2. Statementes.sql to 2. Statements.sql

--- 2. Statementes.sql
+++
@@ -1,5 +0,0 @@
---- STATEMENTS
-/*
-The code below is a SQL statement. A statement is text that the database recognizes as a valid command. Statements always end in a semicolon ;.
-*/
-CREATE TABLE music
