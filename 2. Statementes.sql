b6cca72b763060aa1f4f4eabb30cd7dd52766651
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 14:33:47 -0500

    Create 2. Statementes.sql

---
+++ 2. Statementes.sql
@@ -0,0 +1,5 @@
+--- STATEMENTS
+/*
+The code below is a SQL statement. A statement is text that the database recognizes as a valid command. Statements always end in a semicolon ;.
+*/
+CREATE TABLE music
