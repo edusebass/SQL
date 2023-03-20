a613bf537b4fd5901d9a6b5b85127c82e881b9e8
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 14:20:32 -0500

    Create 1. Intro.sql

---
+++ 1. Intro.sql
@@ -0,0 +1,7 @@
+/*
+                                      SQL
+SQL, Structured Query Language, is a programming language designed to manage data stored in relational databases. 
+SQL operates through simple, declarative statements. This keeps data accurate and secure, and helps maintain the integrity of databases, regardless of size.
+*/
+
+SELECT * FROM celebs;
