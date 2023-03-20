9fe155fa7906c2cd5df3d658083b8b9af67e4f83
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-03-20 14:30:27 -0500

    Update 1. Intro.sql

--- 1. Intro.sql
+++ 1. Intro.sql
@@ -1,7 +1,12 @@
 /*
-                                      SQL
+                          SQL
 SQL, Structured Query Language, is a programming language designed to manage data stored in relational databases. 
 SQL operates through simple, declarative statements. This keeps data accurate and secure, and helps maintain the integrity of databases, regardless of size.
 */
-
+--- Let’s begin by entering a SQL command.
 SELECT * FROM celebs;
+
+/*
+A relational database is a database that organizes information into one or more tables. Here, the relational database contains one table.
+A table is a collection of data organized into rows and columns. Tables are sometimes referred to as relations. Here the table is celebs.
+*/
